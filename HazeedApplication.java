3066ba06bce0a55ab6d105cfec707d3241e04c64
hazeed <115856210+hazeed@users.noreply.github.com>
2022-10-19 14:53:57 +0530

    Add files via upload
    test class added

---
+++ HazeedApplication.java
@@ -0,0 +1,13 @@
+package com.acheiversit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HazeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HazeedApplication.class, args);
+	}
+
+}
